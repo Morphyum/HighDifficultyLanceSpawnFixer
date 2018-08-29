c9c4f067405afbaa84dbeca778e5fd3b0f3dae11
Morphyum <zion@c-schwabe.de>
2018-08-29 17:06:43 +0200

    1.0

---
+++ HighDifficultyLanceSpawnFixer/HighDifficultyLanceSpawnFixer/HighDifficultyLanceSpawnFixer.cs
@@ -0,0 +1,47 @@
+﻿using BattleTech.Data;
+using BattleTech.Framework;
+using Harmony;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighDifficultyLanceSpawnFixer
+{
+    public class HighDifficultyLanceSpawnFixer {
+        internal static string ModDirectory;
+        public static void Init(string directory, string settingsJSON) {
+            ModDirectory = directory;
+            var harmony = HarmonyInstance.Create("de.morphyum.LanceSpawnFixer");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public static void LogLine(string line) {
+            string filePath = $"{ModDirectory}/Log.txt";
+            (new FileInfo(filePath)).Directory.Create();
+            using (StreamWriter writer = new StreamWriter(filePath, true)) {
+                writer.WriteLine(line + Environment.NewLine + "Date :" + DateTime.Now.ToString());
+                writer.WriteLine(Environment.NewLine + "-----------------------------------------------------------------------------" + Environment.NewLine);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(LanceOverride))]
+    [HarmonyPatch("SelectLanceDefFromList")]
+    public static class SimGameState_CreateMechArmorModifyWorkOrder_Patch {
+        public static void Prefix(ref int ___MAX_DIFF_DIVERGENCE) {
+            ___MAX_DIFF_DIVERGENCE = 50;
+        }
+
+        public static void Postfix(LanceDef_MDD __result, int requestedDifficulty, int ___MAX_DIFF_DIVERGENCE) {
+            if (__result == null) {
+                HighDifficultyLanceSpawnFixer.LogLine("No Lance found");
+                HighDifficultyLanceSpawnFixer.LogLine("Diff: " + requestedDifficulty);
+                HighDifficultyLanceSpawnFixer.LogLine("Conver: " + ___MAX_DIFF_DIVERGENCE);
+            }
+        }
+    }
+}
