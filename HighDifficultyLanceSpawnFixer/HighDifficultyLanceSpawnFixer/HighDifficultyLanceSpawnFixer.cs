f895b11a253fe6ff7ab3af6ed6db9f2c1d4a97a5
Morphyum <zion@c-schwabe.de>
2018-08-29 17:07:03 +0200

    *cleanup

--- HighDifficultyLanceSpawnFixer/HighDifficultyLanceSpawnFixer/HighDifficultyLanceSpawnFixer.cs
+++ HighDifficultyLanceSpawnFixer/HighDifficultyLanceSpawnFixer/HighDifficultyLanceSpawnFixer.cs
@@ -2,12 +2,8 @@
 using BattleTech.Framework;
 using Harmony;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HighDifficultyLanceSpawnFixer
 {
